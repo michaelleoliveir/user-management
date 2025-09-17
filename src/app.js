7ba9a32e392cd69b4646980b9cb9bb0d46fed4e7
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-09-16 22:01:19 -0300

    feat: implement user management API with CRUD operations and update tests

---
+++ src/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+const UserRepository = require('./userRepository');
+const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+
+let userRepository;
+let client;
+let connected = false;
+
+app.use(async (req, res, next) => {
+    if (!connected) {
+        const uri = process.env.MONGO_URI;
+        client = new MongoClient(uri);
+        await client.connect();
+
+        const collection = client.db('users_db').collection('users');
+
+        userRepository = new UserRepository(collection);
+
+        connected = true;
+    };
+
+    next();
+})
+
+app.get('/users', async (request, response) => {
+    try {
+        const users = await userRepository.findAllUsers();
+        response.status(200).json(users);
+    } catch (error) {
+        if(error.message === 'Empty list') 
+            return response.status(200).json([])
+    }
+});
+
+app.get('/users/:id', async (request, response) => {
+    const user = await userRepository.findOneById(new ObjectId(request.params.id));
+    response.json(user);
+});
+
+app.post('/users', async (request, response) => {
+    const user = await userRepository.insertUser(request.body);
+    response.status(201).json(user)
+});
+
+module.exports = app
--- src/app.spec.js
+++ src/app.spec.js
@@ -1,14 +1,88 @@
+const request = require('supertest');
+const app = require('./app');
+const UserRepository = require('./userRepository');
+const { MongoClient, ObjectId } = require("mongodb");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 describe('UserAPI', () => {
-    
+
+    let userRepository;
+    let collection;
+    let client;
+
+    beforeAll(async () => {
+        let uri = process.env.MONGO_URI;
+        client = new MongoClient(uri);
+        await client.connect(); // connecting with database
+
+        collection = client.db('users_db').collection('users');
+
+        userRepository = new UserRepository(collection);
+    });
+
+    afterAll(async () => {
+        await client.close();
+    });
+
+    beforeEach(async () => {
+        await collection.deleteMany({});
+    })
+
     describe('/users', () => {
 
         describe('GET /', () => {
-            test.todo('Deve retornar uma lista vazia de usuários');
-            test.todo('Deve retornar uma lista completa de usuários');
+            test('Deve retornar uma lista vazia de usuários', async () => {
+                const response = await request(app).get('/users');
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toStrictEqual([]);
+            });
+
+            test('Deve retornar uma lista completa de usuários', async () => {
+                await userRepository.insertUser({
+                    name: 'Michaelle Oliveira',
+                    email: 'michaelle@teste.com'
+                });
+
+                await userRepository.insertUser({
+                    name: 'Adelino Lopes',
+                    email: 'adelino@teste.com'
+                })
+
+                const response = await request(app).get('/users');
+
+                expect(response.statusCode).toBe(200);
+                expect(response.body[0]).toEqual(expect.objectContaining({
+                    name: 'Michaelle Oliveira',
+                    email: 'michaelle@teste.com'
+                }));
+                expect(response.body[1]).toEqual(expect.objectContaining({
+                    name: 'Adelino Lopes',
+                    email: 'adelino@teste.com'
+                }));
+            });
         });
 
         describe('POST /', () => {
-            test.todo('Deve incluir um usuário no banco de dados');
+            test('Deve incluir um usuário no banco de dados', async () => {
+                const response = await request(app).post('/users').send({
+                    name: 'Michaelle Oliveira',
+                    email: 'michaelle@teste.com'
+                });
+
+                const users = await userRepository.findAllUsers();
+
+                expect(response.statusCode).toBe(201);
+                expect(users).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            name: 'Michaelle Oliveira',
+                            email: 'michaelle@teste.com'
+                        })
+                    ]))
+            });
+
             test.todo('Não deve permitir a inclusão de usuários com emails duplicados');
         });
 
@@ -17,7 +91,22 @@ describe('UserAPI', () => {
     describe('/users/:id', () => {
 
         describe('GET /', () => {
-            test.todo('Deve retornar os dados de um usuário');
+            test('Deve retornar os dados de um usuário', async () => {
+                const user = await userRepository.insertUser({
+                    name: 'Michaelle Oliveira',
+                    email: 'michaelle@teste.com'
+                });
+
+                const response = await request(app).get(`/users/${user._id}`);
+
+                expect(response.statusCode).toBe(200);
+                expect(response.body).toEqual(
+                    expect.objectContaining({
+                        name: 'Michaelle Oliveira',
+                        email: 'michaelle@teste.com'
+                    })
+                )
+            });
             test.todo('Deve retornar status code 404 para usuário não existente');
         });
 
