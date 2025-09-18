189be7ef02fc56d4958353cae839ed6ba75513da
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-09-17 21:09:46 -0300

    feat: add error handling and testing for user operations

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.use(async (req, res, next) => {
         await client.connect();
 
         const collection = client.db('users_db').collection('users');
+        await collection.createIndex({ "email": 1 }, { unique: true })
 
         userRepository = new UserRepository(collection);
 
@@ -36,7 +37,7 @@ app.get('/users', async (request, response) => {
         response.status(200).json(users);
     } catch (error) {
         if (error.message === 'Empty list')
-            return response.status(200).json([])
+            return response.status(400).json([])
     }
 });
 
@@ -45,13 +46,55 @@ app.get('/users/:id', async (request, response) => {
         const user = await userRepository.findOneById(new ObjectId(request.params.id));
         response.json(user);
     } catch (error) {
-        response.status(404).send()
+        response.status(404).send({
+            message: 'User not found',
+            code: 404
+        })
     }
 });
 
 app.post('/users', async (request, response) => {
-    const user = await userRepository.insertUser(request.body);
-    response.status(201).json(user)
+    const { name, email } = request.body;
+
+    try {
+        const user = await userRepository.insertUser({ name, email });
+        response.status(201).json(user);
+    } catch (error) {
+        if (error.code === 11000) {
+            return response.status(400).send({
+                message: 'Invalid email',
+                code: 400
+            });
+        }
+    }
 });
+
+app.put('/users/:id', async (request, response) => {
+    const data = request.body;
+    const { id } = request.params;
+
+    try {
+        const user = await userRepository.updateUser(id, data);
+        return response.status(200).send(user)
+    } catch (error) {
+        if (error.message === `User with id ${id} does not exist`) {
+            return response.status(404).send();
+        }
+    }
+})
+
+app.delete('/users/:id', async (request, response) => {
+    try {
+        await userRepository.deleteUser(new ObjectId(request.params.id));
+        response.status(204).send({
+            message: 'User deleted',
+            code: 204
+        });
+    } catch (error) {
+        if (error.message === 'It is not possible to delete an inexistent user') {
+            return response.status(404).send(message);
+        }
+    }
+})
 
 module.exports = app
--- src/app.spec.js
+++ src/app.spec.js
@@ -35,7 +35,7 @@ describe('UserAPI', () => {
         describe('GET /', () => {
             test('Deve retornar uma lista vazia de usuários', async () => {
                 const response = await request(app).get('/users');
-                expect(response.statusCode).toBe(200);
+                expect(response.statusCode).toBe(400);
                 expect(response.body).toStrictEqual([]);
             });
 
@@ -83,7 +83,30 @@ describe('UserAPI', () => {
                     ]))
             });
 
-            test.todo('Não deve permitir a inclusão de usuários com emails duplicados');
+            test('Não deve permitir a inclusão de usuários com emails duplicados', async () => {
+                const user1 = await request(app).post('/users').send({
+                    name: 'Michaelle Oliveira',
+                    email: 'michaelle@teste.com'
+                });
+                expect(user1.body).toEqual(
+                    expect.objectContaining({
+                        name: 'Michaelle Oliveira',
+                        email: 'michaelle@teste.com'
+                    })
+                );
+
+                const user2 = await request(app).post('/users').send({
+                    name: 'Lara Raj',
+                    email: 'michaelle@teste.com'
+                });
+                expect(user2.statusCode).toBe(400);
+                expect(user2.body).toEqual(
+                    expect.objectContaining({
+                        message: 'Invalid email',
+                        code: 400
+                    })
+                )
+            });
         });
 
     });
@@ -107,20 +130,71 @@ describe('UserAPI', () => {
                     })
                 )
             });
+
             test('Deve retornar status code 404 para usuário não existente', async () => {
                 const response = await request(app).get('/users/1234567890abcdef1234567z');
                 expect(response.statusCode).toBe(404);
+                expect(response.body).toStrictEqual({
+                    message: 'User not found',
+                    code: 404
+                })
             });
         });
 
         describe('PUT /', () => {
-            test.todo('Deve atualizar os dados de um usuário');
-            test.todo('Deve retornar status code 404 para usuário não existente');
+            test('Deve atualizar os dados de um usuário', async () => {
+                const user = await request(app).post('/users').send({
+                    name: 'Michaelle Oliveira',
+                    email: 'michaelle@teste.com'
+                });
+
+                const updated = await request(app).put(`/users/${user.body._id}`).send({
+                    name: 'Michaelle Maria Silva de Oliveira',
+                    email: 'michaelle@teste.com.br'
+                });
+
+                expect(updated.statusCode).toBe(200);
+                expect(updated.body).toStrictEqual(
+                    expect.objectContaining({
+                        name: 'Michaelle Maria Silva de Oliveira',
+                        email: 'michaelle@teste.com.br'
+                    })
+                )
+            });
+
+            test('Deve retornar status code 404 para usuário não existente', async () => {
+                const invalidId = '64f3c0d8a9f1e2b3c4d5e6f7'
+                const updated = await request(app).put(`/users/${invalidId}`).send({
+                    name: 'Teste',
+                    email: 'teste@teste.com'
+                });
+
+                expect(updated.statusCode).toBe(404)
+            });
         });
 
         describe('DELETE /', () => {
-            test.todo('Deve remover um usuário');
-            test.todo('Deve retornar status code 404 para usuário não existente');
+            test('Deve remover um usuário', async () => {
+                const user = await request(app).post('/users').send({
+                    name: 'Michaelle Oliveira',
+                    email: 'michaelle@teste.com'
+                });
+                expect(user.body).toStrictEqual(
+                    expect.objectContaining({
+                        name: 'Michaelle Oliveira',
+                        email: 'michaelle@teste.com'
+                    })
+                );
+
+                const deletar = await request(app).delete(`/users/${user.body._id}`);
+                expect(deletar.statusCode).toBe(204)
+            });
+
+            test('Deve retornar status code 404 para usuário não existente', async () => {
+                const invalidId = '64f3c0d8a9f1e2b3c4d5e6f7';
+                const deletar = await request(app).delete(`/users/${invalidId}`);
+                expect(deletar.statusCode).toBe(404);
+            });
         });
 
     });
